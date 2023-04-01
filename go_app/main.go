b7770f812b951982c8dd2f2adc0cbca46dc0998c
Patrick Coxall <mr.coxall@mths.ca>
2023-04-01 17:40:40 +0000

    put back old code

--- go_app/main.go
+++ go_app/main.go
@@ -1,18 +1,37 @@
-// Created by: Mr Coxall
-// Created on: Sep 2020
+// Created by: Mariam Kasim
+// Created on: March 2023
 //
-// This program displays, "Hello, World!"
+// This file contains the main function for the go_app application
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	const rate float64 = 0.18
+	var hours float64
+	var pay float64
+	var governmentPay float64
+	var salary float64
 
-	// This function displays, "Hello, World!"
+	// input
+	fmt.Print("Enter Hours: ")
+	fmt.Scanln(&hours)
+	fmt.Print("Enter the salary: ")
+	fmt.Scanln(&salary)
 
-	fmt.Println("Hello, World!")
+	// process
+	pay = (hours * salary) * (1 - rate)
+	governmentPay = (hours * salary) * rate
+	roundedpay := fmt.Sprintf("%.2f", pay)
+	roundedGovernmentPay := fmt.Sprintf("%.2f", governmentPay)
+
+	// output
+	fmt.Println("")
+	fmt.Println("Your pay is: $", roundedpay)
+	fmt.Println("Your government pay is: $", roundedGovernmentPay)
 
 	fmt.Println("\nDone.")
-
 }
