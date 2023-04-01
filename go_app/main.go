e371ea812c1bfc7d5dd9a685d307638190f1c4de
Patrick Coxall <mr.coxall@mths.ca>
2023-04-01 17:35:46 +0000

    now with the Hello, World program

---
+++ go_app/main.go
@@ -0,0 +1,18 @@
+// Created by: Mr Coxall
+// Created on: Sep 2020
+//
+// This program displays, "Hello, World!"
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// This function displays, "Hello, World!"
+
+	fmt.Println("Hello, World!")
+
+	fmt.Println("\nDone.")
+
+}
